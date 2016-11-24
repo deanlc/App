0b2224f9bd1050b6c85b16011d17f68620c1cae7
Dean <dean@MacBook-Pro.local>
2016-11-24 21:07:55 +0000

    .

--- js/show.js
+++ js/show.js
@@ -1,53 +1,36 @@
-function makeListItemHTML (data, index) 
-{
-  /*
-    This function creates some nice HTML around data for the #home section
-    Return something like this:
-    <li>
-        <img src="https://ma.tteo.me/assets/surprise.png">
-        <h2>Matteo</h2>
-    </li>
-  */
-
-  // li = List Item
-   var li  = '<li id="' + index + '">'
-  + '<img src="' + data.image + '">' 
-  + '<h2>' + data.name + '</h2>' 
-  + '<p>' + data.about + '</p>'
-  + '</li><hr>'
-
-  return li;        
+function makeListItemHTML(data, index) {
+    /*
+      This function creates some nice HTML around data for the #home section
+      Return something like this:
+      <li>
+          <img src="https://ma.tteo.me/assets/surprise.png">
+          <h2>Matteo</h2>
+      </li>
+    */
+    // li = List Item
+    var li = '<li id="' + index + '">' + '<img src="' + data.image + '">' + '<h2>' + data.name + '</h2><br>' + '<p>' + data.about + '</p>' + '</li><hr>'
+    return li;
 }
 
-function makeDetailsHTML (data) 
-{
-  /*
-    This function creates some nice HTML around data for the #details section
-    Return something like this:
-    <h2>Matteo</h2>
-    <img src="https://ma.tteo.me/assets/surprise.png">
-    <p>I teach people aged 6 to 60+ how to be creative with code.
-    </p>
-    <a class="contact button">Contact Matteo</a>
-  */
-
-  var html = '<h2>' + data.name  + '</h2><br>' 
-  + '<img src="' + data.image + '">' 
-  + '<p>About: ' + data.about + '</p>'
-  + '<p>Skills: ' + data.skills + '</p>'
-  + '<button class="contact button" href="mailto:' + data.email + '">Contact ' + data.name + '</button>'
-
-  return html;        
+function makeDetailsHTML(data) {
+    /*
+      This function creates some nice HTML around data for the #details section
+      Return something like this:
+      <h2>Matteo</h2>
+      <img src="https://ma.tteo.me/assets/surprise.png">
+      <p>I teach people aged 6 to 60+ how to be creative with code.
+      </p>
+      <a class="contact button">Contact Matteo</a>
+    */
+    var html = '<h2>' + data.name + '</h2><br>' + '<img src="' + data.image + '">' + '<p>About: ' + data.about + '</p>' + '<p>Skills: ' + data.skills + '</p>' + '<button type="submit" class="contact button" href="mailto:' + data.email + '">Contact ' + data.name + '</button>'
+    return html;
 }
 
-
-function showList (dataList, interfaceList) 
-{
+function showList(dataList, interfaceList) {
     // update the ul content with the result of makeListHTML(list)
     // html is a jQuery function
-    interfaceList.html( makeListHTML(dataList) ); 
+    interfaceList.html(makeListHTML(dataList));
 }
-
 /*function showDetails (data) 
 {
   var $info = $('.info'); // tell jQuery to select the <div class="info"></div>
@@ -58,24 +41,18 @@ function showList (dataList, interfaceList)
   $('#home').hide();
   $('#details').show();
 }*/
-
-function makeListHTML (list) 
-{
-  var html = ''; // empty for now, we'll add HTML as we loop through the list 
-  var total = list.length;
-
-  // loop through list
-  var counter = 0;
-  while (counter < total) 
-  {
-    var data = list[counter];
-    var li = makeListItemHTML(data, counter);
-    
-    // add the list item to the html
-    html += li;
-    
-    // update the counter, to avoid infinite loops!
-    counter = counter + 1;
-  }
-  return html;
+function makeListHTML(list) {
+    var html = ''; // empty for now, we'll add HTML as we loop through the list 
+    var total = list.length;
+    // loop through list
+    var counter = 0;
+    while (counter < total) {
+        var data = list[counter];
+        var li = makeListItemHTML(data, counter);
+        // add the list item to the html
+        html += li;
+        // update the counter, to avoid infinite loops!
+        counter = counter + 1;
+    }
+    return html;
 }
