470d2b200e9ae86546573ada4ea28dccdfd3d712
Dean <dean@MacBook-Pro.local>
2016-11-30 12:42:19 +0000

    .

--- js/main.js
+++ js/main.js
@@ -21,6 +21,14 @@ var results_message = $('#results_message');
 var resultsLI = jQuery('#results li');
 var search_term = $('#search_term');
 var person_1 = $('#person_1');
+var signUpButton = $('#sign_up_button');
+var signup = $('#sign_up');
+var signUpStudent = $('#studentSignUpButton');
+var signUpJobsButton = $('#jobSignUpButton');
+var signUpJobs = $('#jobs_sign_up');
+var peopleSignUp = $('#people_sign_up');
+var signUpHome = $('#sign_up_home');
+
 // fade in home
 home.hide();
 home.fadeIn(500);
@@ -32,6 +40,10 @@ logo_link.click(function () {
     search.hide();
     jobs.hide();
     search_jobs.hide();
+    signup.hide();
+    signUpHome.hide();
+    peopleSignUp.hide();
+    signUpJobs.hide();
 });
 students_button.click(function () {
     home.hide();
@@ -133,6 +145,13 @@ find_jobs.click(function () {
     });
 });
 
+signUpButton.click(function () {
+    results.hide();
+    signUpHome.fadeIn(500);
+    signup.fadeIn(500);
+});
+
+
 function showDetails(data, interfaceElement) {
     var detailsHTML = makeDetailsHTML(data)
     interfaceElement.html(detailsHTML)
@@ -168,9 +187,72 @@ back_results.click(function () {
     results.show();
     details.hide();
 });
+
+signUpStudent.click(function () {
+    peopleSignUp.fadeIn(500);
+    signUpHome.hide();
+});
+signUpJobsButton.click(function () {
+    signUpJobs.fadeIn(500);
+    signUpHome.hide();
+});
+
+
+
 //person_1.click( function () 
 //{
 //    results.hide();
 //    details.show();
 //    
 //});
+
+var databaseListJob = database.ref( 'jobs' );
+
+// use jQuery to select the <form>
+var form = jQuery( 'form' );
+
+// we'll use jQuery to grab the data from the <form> and store it inside dataUnit
+var dataUnit = {}; 
+
+// when you submit the form...
+form.submit( function( event ) 
+{
+  var today = new Date();
+  /*
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+
+  var yyyy = today.getFullYear();
+  if(dd<10){
+    dd='0'+dd
+  } 
+  if(mm<10){
+    mm='0'+mm
+  } 
+  today = dd+'/'+mm+'/'+yyyy;
+  */
+  document.getElementById("date").value = today;
+  
+  // stop this from reloading
+  event.preventDefault();
+
+  // put the form data inside dataUnit 
+  dataUnit = form.serializeJSON( {parseBooleans: true, parseNumbers: true, checkboxUncheckedValue: "false"} );
+
+  // log what's inside dataUnit
+  console.log( dataUnit );
+
+  // send dataUnit to databaseListJob
+  databaseListJob.push( dataUnit )
+  .then(function() 
+  {
+    alert('Success!');
+    // form[0].reset(); // un-comment this line if you want the form to be cleared from previous values
+  })
+  .catch(function(error) 
+  {
+    console.error( error );
+    alert('Something went wrong, try again!');
+  });
+  
+});
